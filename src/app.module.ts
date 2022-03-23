760caf83f9e92e77dc1d1ea6f26d34935ca54f5d
WayneJr <40968414+WayneJr@users.noreply.github.com>
2022-03-23 09:48:51 +0100

    feat: authentication completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     DatabaseModule, 
-    ConfigModule.forRoot({ isGlobal: true })
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request, Body, Logger } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UserDto } from '../users/dto/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    // @UseGuards(AuthGuard('local'))
+    @Post('login')
+    async login(@Body() user: UserDto) {
+        return await this.authService.login(user);
+    }
+
+    @Post('signup')
+    async signUp(@Body() user: UserDto) {
+        return await this.authService.create(user);
+    }
+}
